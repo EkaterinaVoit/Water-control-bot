be995f25811228267135e44becfec019e31d2258
sinevaninja <72072001+sinevaninja@users.noreply.github.com>
2021-04-24 14:54:16 +0300

    Update waterbot.py
    Это наш основной код. Он отвечает за вопросы для пользователя и если, тот соглашается пользоваться ботом, активирует отправку сообщений через "raspisanie.py".

--- waterbot.py
+++ waterbot.py
@@ -2,9 +2,10 @@ import telebot
 import datetime
 from datetime import datetime
 from telebot import types
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-print("Current Time =", current_time)
+import schedule
+import time
+import os
+from subprocess import call
 
 bot = telebot.TeleBot("1506364107:AAHCWzAP0Vov1DH8fuSZom_UXcTy5MvzHUg")
 TOKEN = '1506364107:AAHCWzAP0Vov1DH8fuSZom_UXcTy5MvzHUg'
@@ -16,33 +17,36 @@ from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
 
-
 markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 itembtn1 = types.KeyboardButton('да')
 itembtn2 = types.KeyboardButton('нет')
-markup.add(itembtn1, itembtn2)
+knopka3 = types.KeyboardButton('я аква-мен')
+knopka4 = types.KeyboardButton('обезвоживание')
+markup.add(itembtn1, itembtn2, knopka3, knopka4)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "Привет! Я бот, который поможет тебе поддерживать водный баланс! ")
 	bot.send_message(user_id, "Вы готовы начать?", reply_markup=markup)
-
-
-
-def test_send_message():
-        text = 'CI Test Message'
-        bot = telebot.TeleBot(TOKEN)
-        ret_msg = bot.send_message(user_id, text='Добрый вечер! Вы пили воду сегодня?')
-        assert ret_msg.message_id
-        if current_time=="13:45:00":
-            test_send_message()
           
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text.lower() == 'да':
-        bot.send_message(message.from_user.id, 'Отлично! Мы готовы начать!')
-        test_send_message()
-    else:
-        bot.send_message(message.from_user.id, 'Я буду ждать твое "да"! Может, попробуешь еще раз?')
-
+        a = telebot.types.ReplyKeyboardRemove()
+        bot.send_message(message.from_user.id, 'Отлично, начинаем!', reply_markup=a) #запустить код времени
+        call(["python", "raspisanie.py"])
+    elif message.text.lower() == 'нет':
+        a = telebot.types.ReplyKeyboardRemove()
+        bot.send_message(message.from_user.id, 'Вы меня огорчаете(', reply_markup=a)
+    elif message.text.lower() == 'я аква-мен':
+        a = telebot.types.ReplyKeyboardRemove()
+        bot.send_message(user_id, 'Вы супер!', reply_markup=a)
+    elif message.text.lower() == 'обезвоживание':
+        a = telebot.types.ReplyKeyboardRemove()
+        bot.send_message(user_id, 'не умирайте плиз', reply_markup=a)
+           
 bot.polling()
+
+
+
+
