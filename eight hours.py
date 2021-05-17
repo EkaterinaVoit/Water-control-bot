539c9343ec94bba6fef447a6920660d5a8fb4599
Войтович Екатерина <72070049+EkaterinaVoit@users.noreply.github.com>
2021-05-17 13:52:47 +0300

    Update eight hours.py

--- eight hours.py
+++ eight hours.py
@@ -8,8 +8,6 @@ import sys
 from subprocess import call
 
 bot = telebot.TeleBot("1506364107:AAHCWzAP0Vov1DH8fuSZom_UXcTy5MvzHUg")
-TOKEN = '1506364107:AAHCWzAP0Vov1DH8fuSZom_UXcTy5MvzHUg'
-bot = telebot.TeleBot(TOKEN)
 
 args = sys.argv
 user_id_str = args[1]
@@ -27,27 +25,22 @@ markup.add(knopka3, knopka4)
 
 def job():
     bot.send_message(user_id, "Вы уже пили воду сегодня?", reply_markup=markup)
-
 schedule.every().day.at("19:00").do(job)
 
 def good_mo():
     bot.send_message(user_id, "Доброе утро! 🌞 \nПопейте,ожалуйста, воду!")
-
 schedule.every().day.at("10:00").do(good_mo)
 
 def good_day():
     bot.send_message(user_id, "Время обеда, а, значит, пора пить воду! 🕑 ")
-
 schedule.every().day.at("15:00").do(good_day)
 
 def good_ev():
     bot.send_message(user_id, "Вечером тоже нужно пить воду! 💗 ")
-
 schedule.every().day.at("19:28").do(good_ev)
 
 def good_ni():
     bot.send_message(user_id, "Доброй ночи! 🌙 \nПопейте, перед сном, пожалуйста! ")
-
 schedule.every().day.at("23:40").do(good_ni)
 
 while True:
